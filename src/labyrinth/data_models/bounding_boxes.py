70640608a4e88b3547f7fc868d79e5307c972c84
Zack Taylor <zack.taylor@vidrovr.com>
2025-04-16 16:24:41 -0400

    Added bounding boxes

---
+++ src/labyrinth/data_models/bounding_boxes.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# Vidrovr Inc.
+
+from typing import List, Self, Sequence, Tuple
+
+import numpy as np
+from pydantic import BaseModel
+
+
+class X4Y4(BaseModel):
+    """Defines a bounding box with 4 (x, y) coord points."""
+
+    top_left: Tuple[float, float]
+    top_right: Tuple[float, float]
+    bottom_right: Tuple[float, float]
+    bottom_left: Tuple[float, float]
+
+
+class CENTER_XYWH(BaseModel):
+    """Defines a bounding box with x, y center points and width and height."""
+
+    x: float
+    y: float
+    width: float
+    height: float
+
+
+class XYWH(BaseModel):
+    """Defines a bounding box with x, y top left and width and height."""
+
+    x: float
+    y: float
+    width: float
+    height: float
+
+    @classmethod
+    def from_list(cls, bbox: List[float]) -> Self:
+        return cls(x=bbox[0], y=bbox[1], width=bbox[2], height=bbox[3])
+
+    def to_numpy_indices(self) -> Sequence:
+        y0 = int(self.y)
+        y1 = int(self.y + self.height)
+        x0 = int(self.x)
+        x1 = int(self.x + self.width)
+
+        return np.s_[y0:y1, x0:x1, ...]
+
+    def to_xyxy_list(self) -> Tuple[int, int, int, int]:
+        y0 = int(self.y)
+        y1 = int(self.y + self.height)
+        x0 = int(self.x)
+        x1 = int(self.x + self.width)
+
+        return (x0, y0, x1, y1)
+
+    def to_cxywh(self) -> CENTER_XYWH:
+        x = self.x + (self.width / 2.0)
+        y = self.y + (self.height / 2.0)
+        return CENTER_XYWH(x=x, y=y, width=self.width, height=self.height)
+
+
+class XYXY(BaseModel):
+    """Defines a bounding box in xyxy format."""
+
+    left: float
+    top: float
+    right: float
+    bottom: float
+
+
+class YXYX(BaseModel):
+    """Defines a bounding box in yxyx format."""
+
+    top: float
+    left: float
+    bottom: float
+    right: float
