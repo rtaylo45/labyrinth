f41307fb1bc4131f396715a55a12b39319e15c48
Zack Taylor <zack.taylor@vidrovr.com>
2025-04-16 13:39:21 -0400

    Added sample type

--- src/labyrinth/types/__init__.py
+++ src/labyrinth/types/__init__.py
@@ -1,8 +1,10 @@
 from labyrinth.types.array import Array
 from labyrinth.types.images import CHWImage, HWCImage
+from labyrinth.types.samples import Sample
 
 __all__ = (
     "Array",
     "HWCImage",
     "CHWImage",
+    "Sample",
 )
---
+++ src/labyrinth/types/samples.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# Vidrovr Inc.
+
+from __future__ import annotations
+
+from typing import NewType
+
+Sample = NewType("Sample", tuple)
