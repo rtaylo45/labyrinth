03ac2d225ea92705909fb8716df850d6fb300b0f
Zack Taylor <zack.taylor@vidrovr.com>
2025-04-17 11:40:47 -0400

    updated import things

---
+++ src/labyrinth/targets/sprite/__init__.py
@@ -0,0 +1,8 @@
+from labyrinth.targets.sprite.protocol import SpritePlacer, SpriteSampler
+from labyrinth.targets.sprite.sprite_placer import UniformSpritePlacer
+
+__all__ = (
+    "SpritePlacer",
+    "SpriteSampler",
+    "UniformSpritePlacer",
+)
