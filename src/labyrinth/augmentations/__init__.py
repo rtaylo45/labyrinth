a85b95b50fbf5c965ced64ab0c2c8981081d604e
Zack Taylor <zack.taylor@vidrovr.com>
2025-04-18 09:57:28 -0400

    import

--- src/labyrinth/augmentations/__init__.py
+++ src/labyrinth/augmentations/__init__.py
@@ -1,3 +1,4 @@
+from labyrinth.augmentations.methods import DummyAugment
 from labyrinth.augmentations.protocol import ImageAugmentation
 
-__all__ = ("ImageAugmentation",)
+__all__ = ("ImageAugmentation", "DummyAugment")
