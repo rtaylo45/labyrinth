db666a5683ce8e846ae7d96e60ddc4527bde16bd
Zack Taylor <zack.taylor@vidrovr.com>
2025-05-13 13:10:56 -0400

    Added search alg

--- src/labyrinth/utils/sprite.py
+++ src/labyrinth/utils/sprite.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # Vidrovr Inc.
 
+from typing import Sequence
+
 import numpy as np
 from pycocotools.mask import decode, frPyObjects
 
@@ -99,3 +101,134 @@ def place_sprite(
     background_array[patch_idx] = patch
 
     return background_array
+
+
+def _is_converged(image: Array, guess: int, direction: str) -> bool:
+    if direction[0] == "x":
+        try:
+            data_before = image[:, guess - 1]
+        except IndexError:
+            data_before = np.array([0, 0, 0])
+
+        try:
+            data_after = image[:, guess + 1]
+        except IndexError:
+            data_after = np.array([0, 0, 0])
+
+        data = image[:, guess]
+
+    else:
+        try:
+            data_before = image[guess - 1, :]
+        except IndexError:
+            data_before = np.array([0, 0, 0])
+
+        try:
+            data_after = image[guess + 1, :]
+        except IndexError:
+            data_after = np.array([0, 0, 0])
+
+        data = image[guess, :]
+
+    if direction[1] == "-":
+        data_after_bool = False
+        data_bool = False
+        data_before_bool = True
+    else:
+        data_after_bool = True
+        data_bool = False
+        data_before_bool = False
+
+    if (
+        np.all(data_after == 0) == data_after_bool
+        and np.all(data == 0) == data_bool
+        and np.all(data_before == 0) == data_before_bool
+    ):
+        return True
+    else:
+        return False
+
+
+def _run_convergence(min_: int, max_: int, image: Array, direction: str) -> int | None:
+    max_iter = 10
+    converged = False
+    guess = None
+    iter_ = 0
+    while not converged:
+        guess = int((max_ + min_) / 2)
+
+        if direction[0] == "x":
+            data = image[:, guess]
+        else:
+            data = image[guess, :]
+
+        if not np.all(data == 0):
+            converged = _is_converged(image, guess, direction)
+
+            if direction[1] == "-":
+                max_ = guess
+            else:
+                min_ = guess
+        else:
+            if direction[1] == "-":
+                min_ = guess
+            else:
+                max_ = guess
+
+        if min_ == max_ == guess:
+            converged = True
+
+        if iter_ >= max_iter:
+            converged = True
+
+        iter_ += 1
+
+    return guess
+
+
+def min_max_search(image: Array) -> Sequence:
+    alpha = image[:, :, 3]
+    height, width = alpha.shape
+
+    height_mid = height // 2
+    width_mid = width // 2
+
+    options_dict = {
+        "xmin": {"min": 0, "max": width_mid, "direction": "x-", "value": None},
+        "xmax": {"min": width_mid, "max": width, "direction": "x+", "value": None},
+        "ymin": {"min": 0, "max": height_mid, "direction": "y-", "value": None},
+        "ymax": {"min": height_mid, "max": height, "direction": "y+", "value": None},
+    }
+
+    for _, options in options_dict.items():
+        min_, max_ = options["min"], options["max"]
+        value = _run_convergence(min_, max_, image, direction=options["direction"])
+        options["value"] = value
+
+    xmin = options_dict["xmin"]["value"]
+    xmax = options_dict["xmax"]["value"]
+    ymin = options_dict["ymin"]["value"]
+    ymax = options_dict["ymax"]["value"]
+
+    return np.s_[ymin:ymax, xmin:xmax, ...]
+
+
+def bbox_squeeze(image: Array):
+    """Reduces the image size to the smallest width/height which encloses the alpha layer.
+
+    During sprite augmentation, it is possible to artifically increase the size of the sprite
+    image. This probably mainly comes from using the SafeRotate augmentation. In order to keep
+    the bounding box size as small as the object, it is necessary to reduce the mask image size
+    to its smallest size. The fuction applies a search algorithm to find the smallest box which
+    completly encloses the positive alpha layer.
+
+    Args:
+        image: Numpy image array of the mask.
+
+    Returns:
+        reduced_image: Numpy image array of the mask reduced to its smallest size.
+    """
+    idxs = min_max_search(image)
+    reduced_image = image[idxs]
+
+    return reduced_image
