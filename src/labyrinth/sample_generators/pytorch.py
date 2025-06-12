46e59359025e27b532034d681ef5ee9634d62ece
Zack Taylor <zack.taylor@vidrovr.com>
2025-06-12 16:20:32 -0400

    added while + try/except to deal with timeouts

--- src/labyrinth/sample_generators/pytorch.py
+++ src/labyrinth/sample_generators/pytorch.py
@@ -8,9 +8,10 @@ from torchvision.transforms import v2
 from torchvision.tv_tensors import BoundingBoxes
 
 from labyrinth.sample_generators.object_detection import GenerateSample
+from labyrinth.utils.exceptions import TimeoutException
 
 
-def collate_fn(
+def list_collate_fn(
     batch: list[Tuple[torch.Tensor, list[int], list[BoundingBoxes]]],
 ) -> Tuple[list[torch.Tensor], list[list[int]], list[list[BoundingBoxes]]]:
     imgs = [item[0] for item in batch]
@@ -69,9 +70,22 @@ class LabyObjectSet(Dataset):
         """
         if idx >= self._max_samples:
             raise IndexError
-        image, labels, xywh_bboxs = self._sample_generator(
-            label_id=None, timeout=self._timeout
-        )
+
+        no_sample = True
+        image, labels, xywh_bboxs = None, None, None
+        while no_sample:
+            try:
+                image, labels, xywh_bboxs = self._sample_generator(
+                    label_id=None, timeout=self._timeout
+                )
+                no_sample = False
+            except TimeoutException:
+                continue
+
+        # Adding this part because of typing but should never happen
+        if (image is None) or (labels is None) or (xywh_bboxs is None):
+            raise SystemError("Hellow super user! You have fucked up")
+
         h, w, _ = image.shape
         raw_bboxs = [[b.x, b.y, b.width, b.height] for b in xywh_bboxs]  # type: ignore
         tv_bboxs = [
