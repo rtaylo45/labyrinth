36f82c705375f7af965bbf943abc1bb437e0fa53
Zack Taylor <zack.taylor@vidrovr.com>
2026-05-17 19:32:26 -0700

    Added rng to class

--- src/labyrinth/backgrounds/background_generators.py
+++ src/labyrinth/backgrounds/background_generators.py
@@ -12,8 +12,6 @@ from labyrinth.backgrounds.protocol import (
 )
 from labyrinth.types import Array
 
-rng = np.random.default_rng()
-
 
 class SolidBGSampler(BGSampler):
     _color_generator: ColorSampler
@@ -21,6 +19,7 @@ class SolidBGSampler(BGSampler):
     _h_max: int
     _w_min: int
     _w_max: int
+    _rng: np.random.Generator
 
     def __init__(
         self,
@@ -29,16 +28,18 @@ class SolidBGSampler(BGSampler):
         height_max: int = 720,
         width_min: int = 100,
         width_max: int = 1280,
+        seed: int | None = None,
     ):
         self._color_generator = color_generator
         self._h_min = height_min
         self._h_max = height_max
         self._w_min = width_min
         self._w_max = width_max
+        self._rng = np.random.default_rng(seed)
 
     def _sample_size(self) -> tuple[int, int]:
-        width = rng.integers(self._w_min, self._w_max, dtype=int)
-        height = rng.integers(self._h_min, self._h_max, dtype=int)
+        width = self._rng.integers(self._w_min, self._w_max, dtype=int)
+        height = self._rng.integers(self._h_min, self._h_max, dtype=int)
 
         return (height, width)
 
@@ -60,13 +61,17 @@ class SolidBGSampler(BGSampler):
 
 class FolderBGSampler(BGSampler):
     _files: list[str]
+    _rng: np.random.Generator
 
     def __init__(
         self,
         image_folder: str,
         number_of_samples: int | None = None,
         glob_expression: str | None = None,
+        seed: int | None = None,
     ):
+        self._rng = np.random.default_rng(seed)
+
         expression = glob_expression if glob_expression is not None else "*"
         files = glob(f"{image_folder}/{expression}")
 
@@ -74,14 +79,14 @@ class FolderBGSampler(BGSampler):
             raise ValueError(f"No files found in folder. {image_folder}/{expression}")
 
         if number_of_samples is not None:
-            files = list(rng.choice(files, size=number_of_samples))
+            files = list(self._rng.choice(files, size=number_of_samples))
 
         self._files = files
 
     def __call__(
         self,
     ) -> Array:
-        file = rng.choice(self._files)
+        file = self._rng.choice(self._files)
 
         img = Image.open(file)
         img = img.convert("RGB")
